449a1cdf30794480f0201c8ca20fbf72353024b0
Aman kumar <amank0259@gmail.com>
2024-03-31 08:10:20 +0530

    initial commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion';
 
 function Work() {
-    var images = [
-        { url: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/650c5f15978b05dc7f2ae554_14.webp', top: "55%", left: "45%", isActive: true },
-        { url: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b413200259aa2289224594_Frame%204-p-500.png', top: "53%", left: "47%", isActive: false },
-        { url: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/64cc29f414c45ee4f46aa3ba_01-p-500.jpg', top: "51%", left: "49%", isActive: false },
-        { url: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/65f112dec7a59534cca51a3d_portfolioCard_cc-01-p-500.webp', top: "49%", left: "51%", isActive: false },
-        { url: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b2cab86e8df24e528c4441_Frame%201.png', top: "47%", left: "53%", isActive: false },
-    ]
+    const [images, setImages] = useState([
+        { url: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/650c5f15978b05dc7f2ae554_14.webp', top: "52%", left: "40%", isActive: false },
+        { url: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b413200259aa2289224594_Frame%204-p-500.png', top: "61%", left: "42%", isActive: false },
+        { url: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/64cc29f414c45ee4f46aa3ba_01-p-500.jpg', top: "65%", left: "45%", isActive: false },
+        { url: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/65f112dec7a59534cca51a3d_portfolioCard_cc-01-p-500.webp', top: "70%", left: "48%", isActive: false },
+        { url: 'https://assets-global.website-files.com/6334198f239547f2fccd84c1/65b2cab86e8df24e528c4441_Frame%201.png', top: "71%", left: "51%", isActive: false },
+    ])
+    const { scrollYProgress } = useScroll()
+
+    scrollYProgress.on("change", (data) => {
+        function imagesShow(arr) {
+            setImages(prev => (
+                prev.map((item, index) => (
+                    arr.indexOf(index) === -1 ? (
+                        { ...item, isActive: false }
+                    )
+                        : { ...item, isActive: true }
+                ))
+            ))
+        }
+        switch (Math.floor(data * 100)) {
+            case 0:
+                imagesShow([]);
+                break;
+            case 1:
+                imagesShow([0])
+                break;
+            case 2:
+                imagesShow([0, 1])
+                break;
+            case 3:
+                imagesShow([0, 1, 2])
+                break;
+            case 4:
+                imagesShow([0, 1, 2, 3])
+                break;
+            case 5:
+                imagesShow([0, 1, 2, 3, 4])
+                break;
+        }
+    })
+
     return (
         <div className='w-full mt-10'>
             <div className='relative max-w-screen-xl mx-auto '>
