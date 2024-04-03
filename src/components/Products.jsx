8701f3a01497df14ba22a56e857ef73efa5f06af
Aman kumar <amank0259@gmail.com>
2024-04-03 20:55:37 +0530

    add animations

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import Product from './Product'
 import { motion } from 'framer-motion';
+import arq from '../assets/Arqitel.webm';
+import ttr from '../assets/TTR.webm'
+import webflow from '../assets/webflow.mp4'
+import cula from '../assets/Cula.mp4'
 
 function Products() {
     const products = [
@@ -24,10 +28,18 @@ function Products() {
                     animate={{ y: position + `rem` }}
                     transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }}
                     className='window absolute w-[30rem] h-[22rem] bg-white left-[25%] overflow-hidden'>
-                    <motion.div animate={{ y: -position + `rem` }} transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} className=' w-full h-full bg-sky-200'></motion.div>
-                    <motion.div animate={{ y: -position + `rem` }} transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} className=' w-full h-full bg-sky-300'></motion.div>
-                    <motion.div animate={{ y: -position + `rem` }} transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} className=' w-full h-full bg-sky-400'></motion.div>
-                    <motion.div animate={{ y: -position + `rem` }} transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} className=' w-full h-full bg-sky-500'></motion.div>
+                    <motion.div animate={{ y: -position + `rem` }} transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} className=' w-full h-full bg-sky-200'>
+                        <video src={arq} muted="true" autoPlay='true' loop="true"></video>
+                    </motion.div>
+                    <motion.div animate={{ y: -position + `rem` }} transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} className=' w-full h-full bg-sky-300'>
+                        <video src={ttr} muted="true" autoPlay='true' loop="true"></video>
+                    </motion.div>
+                    <motion.div animate={{ y: -position + `rem` }} transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} className=' w-full h-full bg-sky-400'>
+                        <video src={webflow} muted="true" autoPlay='true' loop="true"></video>
+                    </motion.div>
+                    <motion.div animate={{ y: -position + `rem` }} transition={{ ease: [0.76, 0, 0.24, 1], duration: .5 }} className=' w-full h-full bg-sky-500'>
+                        <video src={cula} muted="true" autoPlay='true' loop="true"></video>
+                    </motion.div>
                 </motion.div>
             </div>
         </div>
